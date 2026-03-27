327d556e3a2d3ccf14e007df1b0dde221cdffae2
Robert Novotny <robert.novotny@jetbrains.com>
2026-03-27 16:47:11 +0100

    Create a template skeleton for 'css' module

---
+++ css/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.github.novotnyr.mincssrel"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

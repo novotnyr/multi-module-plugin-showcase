fb9d9eb0ea4e46696f34c03af2762a8af9d79b6a
Robert Novotny <robert.novotny@jetbrains.com>
2026-03-27 16:56:46 +0100

    Depends on a shared module and IntelliJ CSS

--- css/build.gradle.kts
+++ css/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":shared"))
+    intellijPlatform {
+        bundledPlugins(providers.gradleProperty("cssPlatformBundledPlugins").map { it.split(',') })
+    }
     intellijPlatform {
         intellijIdea(providers.gradleProperty("platformVersion"))
     }

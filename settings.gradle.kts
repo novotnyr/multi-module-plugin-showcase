b2545fee1505f8c9e6a874b24f7903766da7cf1c
Robert Novotny <robert.novotny@jetbrains.com>
2026-03-27 17:24:18 +0100

    Declare 'settings' plugin in settings.gradle.kts
    Gradle Catalog dependency versions are deduced from the 'settings' plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "mincssrel"
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.14.0"
 }
 
 include("shared")
